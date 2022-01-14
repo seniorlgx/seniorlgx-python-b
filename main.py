8c96c77e5c6070640a1297aec320f9be75d9a848
SeniorLgx <seniorlgx@learnregex.xyz>
2022-01-14 11:49:46 +0700

    Init Repo

---
+++ main.py
@@ -0,0 +1,12 @@
+
+discord_client_secret = "8dyfuiRyq=vVc3RRr_edRk-fK__JItpZ"
+facebook_access_key = '12345678123456781234567812345678'
+
+integration_secret = 'keytoken1234'
+key_map_provider = 'keytoken1234'
+password = 'admin123456789'
+
+
+
+if __name__ == "__main__":
+    print("facebook_access_key : ", facebook_access_key)
